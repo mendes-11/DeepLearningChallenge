3019f2747f71441f788af9faeb2b519dd930016c
mateusSleite <mateusleite2709@gmail.com>
2024-04-18 00:41:55 -0300

    Tentativa

--- c#/MainForm.cs
+++ c#/MainForm.cs
@@ -1,34 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
-using System.IO;
-using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
 using Newtonsoft.Json;
 
 namespace c_
 {
     public class MainForm : Form
     {
-        private PictureBox drawingPictureBox;
-        private List<List<Point>> letters = new List<List<Point>>();
-        private List<Point> currentDrawingPoints = new List<Point>();
-        private Stack<List<List<Point>>> undoStack = new Stack<List<List<Point>>>();
-        private Stack<List<List<Point>>> redoStack = new Stack<List<List<Point>>>();
-        private bool isDrawing = false;
+        private PictureBox webcamPictureBox;
+        private Button captureButton;
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
         private Label lblRecognizedLetter;
         private HttpClient client = new HttpClient();
-        private Color drawingColor = Color.Black;
-        private int brushSize = 6;
-        private int zoomFactor = 100;
 
         public MainForm()
         {
             InitializeComponents();
+            InitializeWebcam();
         }
 
         private void InitializeComponents()
@@ -36,20 +26,23 @@ namespace c_
             Width = 800;
             Height = 600;
 
-            drawingPictureBox = new PictureBox
+            webcamPictureBox = new PictureBox
             {
                 Location = new Point(10, 10),
                 Size = new Size(760, 500),
-                BackColor = Color.White,
-                Cursor = Cursors.Cross,
+                BackColor = Color.Black,
                 BorderStyle = BorderStyle.Fixed3D
             };
-            drawingPictureBox.MouseDown += DrawingPictureBox_MouseDown;
-            drawingPictureBox.MouseMove += DrawingPictureBox_MouseMove;
-            drawingPictureBox.MouseUp += DrawingPictureBox_MouseUp;
-            drawingPictureBox.Paint += DrawingPictureBox_Paint;
-            drawingPictureBox.MouseWheel += DrawingPictureBox_MouseWheel;
-            Controls.Add(drawingPictureBox);
+            Controls.Add(webcamPictureBox);
+
+            captureButton = new Button
+            {
+                Text = "Capturar Foto",
+                Location = new Point(10, 520),
+                Size = new Size(100, 30)
+            };
+            captureButton.Click += CaptureButton_Click;
+            Controls.Add(captureButton);
 
             lblRecognizedLetter = new Label
             {
@@ -60,281 +53,84 @@ namespace c_
                 BorderStyle = BorderStyle.FixedSingle
             };
             Controls.Add(lblRecognizedLetter);
-
-            Button btnClear = new Button
-            {
-                Text = "Limpar",
-                Location = new Point(300, 560),
-                Size = new Size(100, 30)
-            };
-            btnClear.Click += BtnClear_Click;
-            Controls.Add(btnClear);
-
-            ColorDialog colorDialog = new ColorDialog();
-
-            Button btnSelectColor = new Button
-            {
-                Text = "Selecionar Cor Escura",
-                Location = new Point(10, 560),
-                Size = new Size(150, 30)
-            };
-            btnSelectColor.Click += (sender, e) =>
-            {
-                if (colorDialog.ShowDialog() == DialogResult.OK)
-                {
-                    if (IsColorDark(colorDialog.Color))
-                    {
-                        drawingColor = colorDialog.Color;
-                    }
-                    else
-                    {
-                        MessageBox.Show("Por favor, selecione uma cor escura.");
-                    }
-                }
-            };
-            Controls.Add(btnSelectColor);
-
-            TrackBar trackBarBrushSize = new TrackBar
-            {
-                Minimum = 1,
-                Maximum = 20,
-                Value = 6,
-                Location = new Point(180, 560),
-                Size = new Size(100, 30)
-            };
-            trackBarBrushSize.ValueChanged += (sender, e) =>
-            {
-                brushSize = trackBarBrushSize.Value;
-            };
-            Controls.Add(trackBarBrushSize);
-
-            Button btnUndo = new Button
-            {
-                Text = "Desfazer",
-                Location = new Point(400, 560),
-                Size = new Size(100, 30)
-            };
-            btnUndo.Click += BtnUndo_Click;
-            Controls.Add(btnUndo);
-
-            Button btnRedo = new Button
-            {
-                Text = "Refazer",
-                Location = new Point(500, 560),
-                Size = new Size(100, 30)
-            };
-            btnRedo.Click += BtnRedo_Click;
-            Controls.Add(btnRedo);
-
-            Button btnZoomIn = new Button
-            {
-                Text = "+",
-                Location = new Point(610, 560),
-                Size = new Size(30, 30)
-            };
-            btnZoomIn.Click += (sender, e) =>
-            {
-                ZoomIn();
-            };
-            Controls.Add(btnZoomIn);
-
-            Button btnZoomOut = new Button
-            {
-                Text = "-",
-                Location = new Point(650, 560),
-                Size = new Size(30, 30)
-            };
-            btnZoomOut.Click += (sender, e) =>
-            {
-                ZoomOut();
-            };
-            Controls.Add(btnZoomOut);
         }
 
-        private void DrawingPictureBox_MouseDown(object sender, MouseEventArgs e)
+        private void InitializeWebcam()
         {
-            isDrawing = true;
-            currentDrawingPoints = new List<Point>();
-            currentDrawingPoints.Add(e.Location);
-        }
-
-        private void DrawingPictureBox_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isDrawing)
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevices.Count == 0)
             {
-                currentDrawingPoints.Add(e.Location);
-                drawingPictureBox.Invalidate();
-            }
-        }
-
-        private async void DrawingPictureBox_MouseUp(object sender, MouseEventArgs e)
-        {
-            isDrawing = false;
-            letters.Add(new List<Point>(currentDrawingPoints));
-            undoStack.Push(new List<List<Point>>(letters));
-            redoStack.Clear();
-            drawingPictureBox.Invalidate();
-
-            if (currentDrawingPoints.Count > 1)
-            {
-                await RecognizeLetter();
+                MessageBox.Show("Nenhuma webcam encontrada.");
+                return;
             }
 
-            currentDrawingPoints.Clear();
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
         }
 
-        private void DrawingPictureBox_Paint(object sender, PaintEventArgs e)
+        private void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
-            using (var pen = new Pen(drawingColor, brushSize))
-            {
-                pen.StartCap = pen.EndCap = LineCap.Round;
+            webcamPictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
 
-                foreach (var letterPoints in letters)
+        private async void CaptureButton_Click(object sender, EventArgs e)
+        {
+            if (webcamPictureBox.Image == null)
+            {
+                MessageBox.Show("Nenhuma imagem para capturar.");
+                return;
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                webcamPictureBox.Image.Save(stream, ImageFormat.Jpeg);
+                var imageData = stream.ToArray();
+
+                try
                 {
-                    if (letterPoints.Count > 1)
+                    using (var fileContent = new ByteArrayContent(imageData))
                     {
-                        for (int i = 1; i < letterPoints.Count; i++)
+                        fileContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
+                        using (var content = new MultipartFormDataContent())
                         {
-                            e.Graphics.DrawLine(pen, letterPoints[i - 1], letterPoints[i]);
-                        }
-                    }
-                }
+                            content.Add(fileContent, "file", "webcam.jpg");
 
-                if (isDrawing && currentDrawingPoints.Count > 1)
-                {
-                    for (int i = 1; i < currentDrawingPoints.Count; i++)
-                    {
-                        e.Graphics.DrawLine(pen, currentDrawingPoints[i - 1], currentDrawingPoints[i]);
-                    }
-                }
-            }
-        }
+                            var response = await client.PostAsync("http://localhost:5000/upload", content);
+                            var responseString = await response.Content.ReadAsStringAsync();
+                            var jsonResponse = JsonConvert.DeserializeObject<dynamic>(responseString);
+                            var phrasesList = jsonResponse?.phrases?.ToObject<List<string>>() ?? new List<string>();
+                            var phrases = string.Join(" ", phrasesList);
 
-        private void BtnClear_Click(object sender, EventArgs e)
-        {
-            ClearCanvas();
-        }
-
-        private void ClearCanvas()
-        {
-            letters.Clear();
-            undoStack.Clear();
-            redoStack.Clear();
-            drawingPictureBox.Invalidate();
-        }
-
-        private void BtnUndo_Click(object sender, EventArgs e)
-        {
-            if (undoStack.Count > 0)
-            {
-                redoStack.Push(new List<List<Point>>(letters));
-                letters = new List<List<Point>>(undoStack.Pop());
-                drawingPictureBox.Invalidate();
-            }
-        }
-
-        private void BtnRedo_Click(object sender, EventArgs e)
-        {
-            if (redoStack.Count > 0)
-            {
-                undoStack.Push(new List<List<Point>>(letters));
-                letters = new List<List<Point>>(redoStack.Pop());
-                drawingPictureBox.Invalidate();
-            }
-        }
-
-        private async Task RecognizeLetter()
-        {
-            using (Bitmap bmp = new Bitmap(drawingPictureBox.Width, drawingPictureBox.Height))
-            {
-                drawingPictureBox.DrawToBitmap(bmp, drawingPictureBox.ClientRectangle);
-                using (var stream = new MemoryStream())
-                {
-                    bmp.Save(stream, ImageFormat.Jpeg);
-                    var imageData = stream.ToArray();
-
-                    await Task.Run(async () =>
-                    {
-                        try
-                        {
-                            using (var fileContent = new ByteArrayContent(imageData))
-                            {
-                                fileContent.Headers.ContentType = new MediaTypeHeaderValue("image/jpeg");
-                                using (var content = new MultipartFormDataContent())
-                                {
-                                    content.Add(fileContent, "file", "drawing.jpg");
-
-                                    var response = await client.PostAsync("http://localhost:5000/upload", content);
-                                    var responseString = await response.Content.ReadAsStringAsync();
-                                    var jsonResponse = JsonConvert.DeserializeObject<dynamic>(responseString);
-                                    var phrasesList = jsonResponse?.phrases?.ToObject<List<string>>() ?? new List<string>();
-                                    var phrases = string.Join(" ", phrasesList);
-
-                                    lblRecognizedLetter.Invoke((MethodInvoker)delegate
-                                    {
-                                        if (phrases.Length > 1)
-                                        {
-                                            lblRecognizedLetter.Text = $"Letra Reconhecida: {phrases.Substring(1)}";
-                                        }
-                                        else
-                                        {
-                                            lblRecognizedLetter.Text = "Nenhuma letra reconhecida.";
-                                        }
-                                    });
-                                }
-                            }
-                        }
-                        catch (Exception ex)
-                        {
                             lblRecognizedLetter.Invoke((MethodInvoker)delegate
                             {
-                                lblRecognizedLetter.Text = $"Erro ao processar: {ex.Message}";
+                                if (phrases.Length > 1)
+                                {
+                                    lblRecognizedLetter.Text = $"Letra Reconhecida: {phrases.Substring(1)}";
+                                }
+                                else
+                                {
+                                    lblRecognizedLetter.Text = "Nenhuma letra reconhecida.";
+                                }
                             });
                         }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblRecognizedLetter.Invoke((MethodInvoker)delegate
+                    {
+                        lblRecognizedLetter.Text = $"Erro ao processar: {ex.Message}";
                     });
                 }
             }
         }
 
-        private void DrawingPictureBox_MouseWheel(object sender, MouseEventArgs e)
+        protected override void OnFormClosing(FormClosingEventArgs e)
         {
-            if (e.Delta > 0)
-            {
-                ZoomIn();
-            }
-            else
-            {
-                ZoomOut();
-            }
-        }
-
-        private void ZoomIn()
-        {
-            if (zoomFactor < 200)
-            {
-                zoomFactor += 10;
-                drawingPictureBox.Width = (int)(800 * zoomFactor / 100.0);
-                drawingPictureBox.Height = (int)(600 * zoomFactor / 100.0);
-                drawingPictureBox.Invalidate();
-            }
-        }
-
-        private void ZoomOut()
-        {
-            if (zoomFactor > 20)
-            {
-                zoomFactor -= 10;
-                drawingPictureBox.Width = (int)(800 * zoomFactor / 100.0);
-                drawingPictureBox.Height = (int)(600 * zoomFactor / 100.0);
-                drawingPictureBox.Invalidate();
-            }
-        }
-
-        private bool IsColorDark(Color color)
-        {
-            double luminance = (0.2126 * color.R + 0.7152 * color.G + 0.0722 * color.B) / 255;
-            double darkColorThreshold = 0.4;
-            return luminance < darkColorThreshold;
+            base.OnFormClosing(e);
+            if (videoSource != null && videoSource.IsRunning)
+                videoSource.Stop();
         }
     }
 }
